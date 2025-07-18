4415ae94583c588a93b6ca1cc34c1086655eac42
Vitoria Comiran <vicomiran@gmail.com>
2025-07-17 21:06:05 -0300

    básico

---
+++ variaveis/escopo.js
@@ -0,0 +1,13 @@
+/*
+Escopo é o lugar unde determinada variável é acessível ou visível.
+
+Define o contexto no qual uma variável pode ser referenciada e modificada
+
+global: variáveis declaradas fora de qualquer função ou bloco de código
+-> var
+
+bloco: acessíveis apenas dentro do bloco de código onde foram declaradas
+-> let e const
+
+
+*/
---
+++ variaveis/variaveis.js
@@ -0,0 +1,17 @@
+// declara uma variável sem valor
+
+var user 
+
+console.log(user)
+
+// declara uma variável com valor
+
+var email = "vitoria@email.com"
+console.log(email)
+
+// substitui o valor da variável
+
+email = "joao@email.com"
+console.log(email) 
+
+// email passa a ter novo valor
