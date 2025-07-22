403a47b207e71623dd01f9b8dbe618126d93039c
Vitoria Comiran <vicomiran@gmail.com>
2025-07-21 23:25:42 -0300

    exercicios funções

---
+++ exercicios_function/exercicio1.js
@@ -0,0 +1,11 @@
+// escreva uma função que receba um nome e imprima uma saudação
+// entrada: vitória
+// saudação: olá vitória
+
+
+function saudar(nome){
+    console.log("Olá", nome)
+}
+
+saudar("Vitória")
+
